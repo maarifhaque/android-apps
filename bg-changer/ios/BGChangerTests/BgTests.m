9364db01e75335e50b9b423823a5d16bfb6be2f2
Maarif <maarifuh@gmail.com>
2021-02-05 21:54:36 +0530

    Renamed App

--- bg-changer/ios/BGChangerTests/BgTests.m
+++ bg-changer/ios/BGChangerTests/BgTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface BgTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation BgTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- diceRoller/ios/DiceRollerTests/DiceTests.m
+++ diceRoller/ios/DiceRollerTests/DiceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface diceRollerTests : XCTestCase
+@interface DiceTests : XCTestCase
 
 @end
 
-@implementation diceRollerTests
+@implementation DiceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
